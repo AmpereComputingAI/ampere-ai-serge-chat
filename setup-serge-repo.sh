698fcd6e51f42d3fc217d87b6457c1d8cdc40102
Iyappan Subramanian <isubramanian@amperecomputing.com>
2024-05-29 16:42:28 +0000

    bump llama.aio to 1.2.3 and llama-cpp-python to 0.2.75

--- setup-serge-repo.sh
+++ setup-serge-repo.sh
@@ -3,18 +3,14 @@
 # Download serge repo
 root_dir=`pwd`
 repo_dir='serge'
-tag_name="0.9.0"
 url="https://github.com/serge-chat/$repo_dir.git"
+tag_name="1c6a077" # uses llama-cpp-python == 0.2.75
 
 rm -rf $repo_dir
-
-if [ ! -d "$repo_dir" ] ; then
-    git clone $url
-    pushd $repo_dir
-    git checkout -b $tag_name $tag_name
-    popd
-fi
-
+git clone $url
+pushd $repo_dir
+git checkout -b $tag_name $tag_name
+popd
 
 # Copy logo files
 pushd $repo_dir
